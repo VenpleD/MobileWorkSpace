136ffb4ad6843bb7956390609d3056c0bb5fdbc3
Venple <venple@VenpledeMacBook-Pro.local>
2022-11-19 15:08:23 +0800

    faet：第二节课MVVM

--- iOS Demo/CS193p/CS193p/ContentView.swift
+++ iOS Demo/CS193p/CS193p/ContentView.swift
@@ -8,20 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var viewModel: EmojiMemoryGame
     var body: some View {
-        return ZStack(content:{
-            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-            RoundedRectangle(cornerRadius: 10.0).stroke()
-            Text("👻")
-        })
-            .foregroundColor(Color.orange)
-            .font(Font.largeTitle)
-            .padding()
+        HStack{
+            ForEach(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    viewModel.choose(card: card)
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    var body: some View {
+        ZStack{
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke()
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
--- iOS Demo/CS193p/CS193p/EmojiMemoryGame.swift
+++ iOS Demo/CS193p/CS193p/EmojiMemoryGame.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    private(set) var model: MemoryGame<String>
+
+protocol Greatness {
+    func isGreaterThan(other: Self) -> Bool
+    /// 这里的Self指的是实现该协议的（任何事物）的类型
+}
+
+
+class EmojiMemoryGame: ObservableObject {
+    private(set) var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🎃", "🕷️"]
+        return MemoryGame(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
     
     func choose(card: MemoryGame<String>.Card) {
-        
+        model.choose(card: card)
     }
 }
